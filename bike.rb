d0c7fb0115cf18aef79c481ed8c41bf64986c8e8
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-03-18 11:05:29 -0700

    Implement Bike abstract class References #1

---
+++ bike.rb
@@ -0,0 +1,41 @@
+
+class Bike
+
+  def initialize
+    raise "Cannot instantiate Bike abstract class"
+  end
+
+  def clean
+    puts "Cleaning..."
+  end
+
+  def adjust_seat
+    puts "Adjusting seat..."
+  end
+
+  def prepare
+    clean
+    adjust
+  end
+
+  def adjust
+    raise "adjust method not implemented in child class"
+  end
+
+  def tail_pack
+    @tail_pack
+  end
+
+  def weekly_rate
+    @weekly_price
+  end
+
+  def daily_rate
+    @daily_price
+  end
+
+  def hourly_rate
+    @hourly_price
+  end
+
+end
